eace16dbf2bdc4f7c50c70ba4e6ae643c7f68516
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-17 02:11:35 +0000

    16章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
+  end
+  
+   def update
+    @user = User.find(params[:id])  #ユーザーの取得
+    @user.update(user_params)       #ユーザーのアップデート
+    redirect_to user_path(@user.id) #ユーザーの詳細ページへのパス  
+   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
