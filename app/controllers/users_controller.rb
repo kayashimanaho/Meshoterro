659a0a4d5b8711bdf49b1172b7e2f8d3de5310d9
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-21 02:23:20 +0000

    Meshiterro終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -8,16 +9,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-   def update
+  def update
     @user = User.find(params[:id])  #ユーザーの取得
     @user.update(user_params)       #ユーザーのアップデート
     redirect_to user_path(@user.id) #ユーザーの詳細ページへのパス  
-   end
+  end
 
 
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
   end
 end
